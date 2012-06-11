5d0dd9345bc9e645d0b33c00a04d4ec4cd6754df
Rio Purnomo <siriokun@gmail.com>
2012-06-11 22:40:10 +0700

    Current slug indicator for specific css styling.
    + show blog title on <title>
    + show current year & blog title on footer

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE HTML>
-<html>
+<html<?php echo ' class="slug-'.basename(get_permalink()).'"'; ?>>
 <head>
 <meta charset="utf-8">
 <title>
@@ -7,6 +7,7 @@
 	/* Print the <title> tag based on what is being viewed. */
 	global $page, $paged;
 	wp_title( '|', true, 'right' );
+	bloginfo( 'name' );
 	?>
 </title>
 <base href="<?php bloginfo('template_url'); ?>/">
@@ -20,6 +21,10 @@
   </a></h1>
 <p>
   <?php bloginfo( 'description' ); ?>
+</p>
+<hr>
+<p><?php echo date('Y'); ?> &copy;
+  <?php bloginfo( 'name' ); ?>
 </p>
 <?php wp_footer(); ?>
 </body>
